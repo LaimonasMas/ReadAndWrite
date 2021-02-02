91533005ec44f0424e2effcf1da491ad6c1b7540
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-02-02 13:57:41 +0200

    removed unused imports

--- src/readwritend/ReadWriteND.java
+++ src/readwritend/ReadWriteND.java
@@ -4,17 +4,12 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class ReadWriteND {
